f0ee1cc014b0aad44a2e7b568124625250521ac2
Wondesem <wseminew50@gmail.com>
2024-01-25 09:21:30 +0300

    adding new line at the end of task 0 and task 1

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,6 @@
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
-<<<<<<< HEAD
+
   return fromJS(object);
-=======
-	return fromJS(object);
->>>>>>> 5f926020c40a9b89c4eab268d07b484e10c63a7f
 }
