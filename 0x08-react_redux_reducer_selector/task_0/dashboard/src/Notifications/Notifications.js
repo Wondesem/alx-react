f15fa9a3af6b862b3403cbac3f8379d9856cb282
Wondesem <wseminew50@gmail.com>
2024-02-09 19:42:58 +0300

    dot included in resolve extension

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,5 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
-import close_icon from '../assets/close-icon.png';
+import close_icon from "../assets/close-icon .png";
 import NotificationItem from './NotificationItem';
 import PropTypes from 'prop-types';
 import NotificationItemShape from './NotificationItemShape';
