88afdf86cf1da4e183347715fec6a4a2386fc58e
wondesem <wseminew50@gmail.com>
2024-07-31 23:21:43 +0300

    task 5 webpack setting and bundling

---
+++ task_5/dashboard/config/webpack.config.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html"),
+      filename: "index.html",
+      inject: true,
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: { loader: 'babel-loader' }
+        },
+        {
+            test: /\.(css)$/,
+            use: ['style-loader','css-loader']
+         },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
