480579488b637463ce13f93e0be3750586e7f6ac
wondesem <wseminew50@gmail.com>
2024-08-04 13:11:05 +0300

    push all things

--- 0x02-react_props/task_1/dashboard/src/Header/Header.test.js
+++ 0x02-react_props/task_1/dashboard/src/Header/Header.test.js
@@ -3,7 +3,10 @@ import {shallow} from 'enzyme'
 
 describe("Header should run without crashing", ()=>{
 const wrapper = shallow(<Header/>)
-it("finds img tag", ()=>{
-    expect(wrapper.find('.App-logo')).toBeDefi(1)
+it("The Header contains at least one img tag", ()=>{
+    expect(wrapper.find('img')).toBeDefined()
+})
+it("The Header renders h1 tags at least once", ()=>{
+    expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toBe(true)
 })
 })
